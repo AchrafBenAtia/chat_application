7f81a8de536870d0a9c65bc884a37549a7dc6590
AchrafBenAtia <achrafbenatia@gmail.com>
2015-03-19 18:19:16 +0100

    conversation history

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -2,15 +2,24 @@
 
 app.controller('ChatController', function($scope, $rootScope, $compile, socket) {
 
-
-  $scope.usersOnlines = {};
-
   var username = document.getElementById('username').value;
-
   var user_id = document.getElementById('user').value;
-
   var msgs = [];
   var count = 0;
+  $scope.usersOnlines = {};
+  $scope.dests = []; //user destinations array
+  $scope.messages = [];
+  $scope.conversations = []; //user Conversations array 
+  var exist = function(tagname, tab) {
+    var i = null;
+    for (i = 0; tab.length > i; i += 1) {
+      if (tab[i] == tagname) {
+        return true;
+      }
+    }
+
+    return false;
+  };
   //load old conversation on click !!!
   $scope.load = function(dest) {
     if (count == 0) {
@@ -37,7 +46,6 @@ app.controller('ChatController', function($scope, $rootScope, $compile, socket) 
       });
       count++;
     }
-
   }
 
   $scope.addbox = function(dest) {
@@ -45,41 +53,54 @@ app.controller('ChatController', function($scope, $rootScope, $compile, socket) 
       if (exist(dest, $scope.dests) == false) {
         angular.element(document.getElementById('chat_box_content')).append($compile("<box destination=" + dest + " ></box>")($rootScope));
         $scope.dests.push(dest);
+            $scope.notif = 0 ;
+
       }
     }
   }
+  $scope.notification = function() {
 
+    $scope.notif = 0;
+  }
 
-
-  //Ne pas afficher l'utilisateur connecté dans la liste des utilisateur  
-
+  //tell socket.io that y're connected 
   socket.emit('iamhere', user_id);
+
+  //get online users List
   socket.on('whoshere', function(data) {
 
     $scope.usersOnlines = data.users;
   });
-  var exist = function(tagname, tab) {
-    var i = null;
-    for (i = 0; tab.length > i; i += 1) {
-      if (tab[i] == tagname) {
-        return true;
-      }
-    }
 
-    return false;
-  };
-  $scope.dests = [];
-  $scope.messages = [];
-  //get messages from other users
-  socket.on('send:message', function(message) {
-    $scope.messages.push(message);
-    console.log($scope.messages);
+  //get conversations history
+  socket.on("load:ConversationHistory", function(data) {
+
+    for (var i = 0; i < data.length; i++) {
+      var user;
+      if (data[i].username1 == username) {
+        user = data[i].username2;
+      } else {
+        user = data[i].username1;
+      }
+
+      var conversation = {
+        intervenant: user,
+        msgs: data[i].messages
+      }
+      $scope.conversations.push(conversation);
+
+    }
 
 
   });
 
-  //send messages to other users 
+  //get messages from other users
+  socket.on('send:message', function(message) {
+    $scope.messages.push(message);
+    $scope.notif = $scope.notif + 1;
+  });
 
+  //send messages to other users 
   $scope.sendMessage = function() {
     socket.emit('send:message', {
       message: $scope.message,
@@ -93,7 +114,6 @@ app.controller('ChatController', function($scope, $rootScope, $compile, socket) 
       date: new Date().toGMTString()
     });
     $scope.message = '';
-
   }
 
 
